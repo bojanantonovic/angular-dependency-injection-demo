fcea3ec81640a5f1f1577e60e300cdfe2d9f4f57
Bojan Antonovic <bojan@antonovic.ch>
2024-11-07 16:33:54 +0100

    removing RouterOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {RandomNumberService} from './random-number-service'
+import { RandomNumberService } from './random-number-service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
